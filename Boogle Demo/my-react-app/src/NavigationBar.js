4a6ceb111db5c52797746ff5525c6d1d257ba1e8
manaxhdanny <dannybao619@gmail.com>
2025-09-28 19:21:39 -0500

    Added Sidebar Functionality

---
+++ Boogle Demo/my-react-app/src/NavigationBar.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import menuBars from './menu_bars.png';
+import { Link } from 'react-router-dom';
+
+function NavigationBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className={isOpen ? 'isOpen' : ''}>
+        <button onClick={() => setIsOpen(!isOpen)} style={{backgroundColor: 'transparent', border: 'none'}}><img src={menuBars} alt="Menu Bars" style={{maxWidth: '60px', height: 'auto'}} /></button>
+        {isOpen && (
+            <>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/addDBData">Add to Database</Link></li>
+                </ul>
+            </>
+        )}
+    </nav>
+  );
+}
+
+export default NavigationBar;
