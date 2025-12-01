a3e711854b00e5ace9f227c8e42290936cc58952
ivan <iliang1222002@gmail.com>
2025-12-01 01:39:38 -0600

    Web Search engine with link and descriptor as well as pagination. Also added a KWICIndex to output all the things in the DB.

--- Boogle Demo/Flask Files/KWIC.py
+++ Boogle Demo/Flask Files/KWIC.py
@@ -1,8 +1,9 @@
 class Alphabetizer:
     #constructor
-    def __init__(self, shiftedSentence = ""):
-        self.shiftedSentence = shiftedSentence
+    def __init__(self, filteredShiftedLines = ""):
+        self.filteredShiftedLines = filteredShiftedLines
 
+<<<<<<< HEAD
     #alphabitize the results of cirular shift
     def alphabetize(self):
         return sorted(self.shiftedSentence, key=lambda s: s.lower())
@@ -36,4 +37,53 @@ class NoiseFilter:
         filtered = [w for w in words if w not in self.noise_words]
         return " ".join(filtered)
 
+=======
+    # alphabitize the results of cirular shift
+    def alphabetizeShifts(self):
+        return sorted(self.filteredShiftedLines, key = str.lower)
+
+class CircularShift:
+
+    # constructor
+    def __init__(self, lineStorageLines):
+
+        # public data members
+        self.lineStorageLines = lineStorageLines
     
+    # public function accessible by Input
+    def setLines(self, lineStorageLines):
+        self.lineStorageLines = lineStorageLines
+
+    # Public Function to shift the words and returns a list of all possible shifts accessible by Alphabetizer
+    def computeCircularShifts(self):
+        shifts = []
+        for line in self.lineStorageLines:
+            lineShifts = []
+            words = line.split()
+
+            for i in range(len(words)):
+                lineShifts.append(" ".join(words[i:len(words)]) + " " + " ".join(words[0: i]))
+            shifts.extend(lineShifts)
+    
+        return shifts
+>>>>>>> 33a1c79f1a6149b829f0dc9659da338988e9d3a9
+    
+class NoiseEliminator:
+
+    # constructor
+    def __init__(self, circularShiftedLines = ""):
+
+        # public data members
+        self.circularShiftedLines = circularShiftedLines
+
+    # Public Function to filter the circular shifted lines to remove filler words
+    def eliminateNoise(self):
+        res = []
+        words_to_ignore = ["a", "and", "as", "in", "is", "of", "on", "the", "to"]
+        
+        for i in range(len(self.circularShiftedLines)):
+            firstWord = self.circularShiftedLines[i].split()[0]
+            if firstWord.lower() not in words_to_ignore:
+                res.append(self.circularShiftedLines[i])
+    
+        return res
